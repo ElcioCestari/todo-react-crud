db35c1124719f2252e81287bb1fc9a1d47723fcf
ElcioCestari <elciotaira@gmail.com>
2022-03-12 15:29:10 -0300

    feat(main): finished crud

--- src/App.js
+++ src/App.js
@@ -21,12 +21,26 @@ function App() {
     setTodoItems(todos);
   };
 
-  const updateTodoItem = (index) => {
+  const completeTodoItem = (index) => {
     const todos = [...todoItems];
     if (!todos[index].complete) {
       todos[index].complete = true;
       setTodoItems(todos);
     }
+  };
+
+  const updateTodoItem = (index) => {
+    const newTodoItems = [...todoItems];
+    const item = newTodoItems[index];
+    let newItem = prompt(`Update ${item.todo}?`, item.todo);
+    let todoObj = { todo: newItem, complete: false };
+    newTodoItems.splice(index, 1, todoObj);
+    if (newItem === null || newItem === "") {
+      return;
+    } else {
+      item.todo = newItem;
+    }
+    setTodoItems(newTodoItems);
   };
 
   return (
@@ -38,6 +52,7 @@ function App() {
           index={index}
           item={item}
           deleteTodoItem={deleteTodoItem}
+          completeTodoItem={completeTodoItem}
           updateTodoItem={updateTodoItem}
         />
       ))}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const TodoItem = ({ item, index, deleteTodoItem, updateTodoItem }) => {
+const TodoItem = ({ item, index, deleteTodoItem, completeTodoItem, updateTodoItem }) => {
   return (
     <div>
       <li>
         {`${item.todo} complete: ${item.complete}`}
         <button onClick={() => deleteTodoItem(index)}>del</button>
+        <button onClick={() => completeTodoItem(index)}>complete</button>
         <button onClick={() => updateTodoItem(index)}>update</button>
       </li>
     </div>
